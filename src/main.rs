45359578f6f25d1476787c5eb28f56b3af481b40
RATIU5 <jcmemmott20@outlook.com>
2024-03-04 07:47:35 -0700

    edit: use new impl on Config struct

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,10 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
     Config { query, file_path }
 }
