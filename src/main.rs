d714d2bdb66b11691f72d0bec34ae514fde68c1c
RATIU5 <jcmemmott20@outlook.com>
2024-03-04 07:39:42 -0700

    edit: return config struct from parse config fn

--- src/main.rs
+++ src/main.rs
@@ -19,3 +19,15 @@ fn main() {
         process::exit(1);
     }
 }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+}
